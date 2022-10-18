09c95acf55f4943d625c79a22db7da70226d56eb
weber <weber122462118@gmail.com>
2022-10-18 17:52:43 +0800

    221018 更新匯入用資料+來源

---
+++ README.md
@@ -0,0 +1,10 @@
+使用資料庫為MSSQL 可直接打開使用
+資料庫預設路徑為:mailbox
+資料表名稱:mailBoxTable
+使用API:dom4j,gson,jaxen,jgoodies,mssql-jdbc-10.2.1.jre8
+
+使用資料:
+政府資料開放平台-信筒(箱)設置地點
+提供機關:中華郵政股份有限公司
+資料版本:2021-06-28 16:49
+https://data.gov.tw/dataset/7964
---
+++ 使用資料/mailBoxSQL.sql
@@ -0,0 +1,20 @@
+--新增資料庫mailbox
+create table mailBoxTable(
+id int primary key,
+classNumber int,
+className varchar(max),
+classOfMailbox int,
+mailboxName varchar(max),
+address varchar(max),
+descripAddress varchar(max),
+mechanism varchar(max),
+phoneNumber Varchar(max),
+remark varchar(max),
+longitude float,
+latitude float
+);
+
+delete from mailBoxTable
+
+select * from mailBoxTable
+
